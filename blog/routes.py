316a14b8474d3af37fa40e1a0c4763ba6157f567
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-07-19 01:15:46 +0100

    Successfully implemented 'profile picture updating feature'

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from blog import app, bcrypt, db
-from flask import  flash, redirect, render_template, url_for
+from flask import  flash, redirect, render_template, url_for, request
 from blog.models import User, Post
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_required, login_user, current_user, logout_user
 
 posts = [
@@ -80,10 +83,32 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    new_image = Image.open(form_picture)
+    new_image.thumbnail(output_size)
+    new_image.save(picture_path)
+    return picture_fn
 
-
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-   image_file = current_user.image_file
-   return render_template("account.html", title="Account",image_file=image_file)
+   form = UpdateAccountForm()
+   if form.validate_on_submit():
+       if form.picture.data:
+           image_file = save_picture(form.picture.data)
+           current_user.image_file = image_file
+       current_user.username = form.username.data
+       current_user.email = form.email.data
+       db.session.commit()
+       flash("Your account has been updated!", category='success')
+       return redirect(url_for('account'))
+   elif request.method == 'GET':
+       form.username.data = current_user.username
+       form.email.data = current_user.email
+   image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+   return render_template("account.html", title="Account", image_file=image_file, form=form)
